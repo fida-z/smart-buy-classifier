baa43b1607419a4454e1e5a188023ff78e007966
fida <sisfida7422@gmail.com>
2026-09-18 05:17:03 +0530

    Corrected NGT life addition error

--- modules/feature_eng.py
+++ modules/feature_eng.py
@@ -1,7 +1,5 @@
 import pandas as pd
-import datetime
 
-curr_year = datetime.date.today().year
 LUXURY_MODELS = 'VOLVO|MERCEDES|AUDI|LEXUS|ROLLS-ROYCE|BENTLEY|PORSCHE|FERRARI|LAMBORGH|ASTON MARTIN|LAND ROVER|RANGE ROVER|BMW 7|BMW X7'
 
 
@@ -16,29 +14,29 @@ def calc_ngt_life(fuel: str, car_age: int) -> int:
 
 def car_builder(carClass) -> pd.DataFrame :
 
-    car_age = curr_year - carClass.year
-    ngt_life = calc_ngt_life(carClass.fuel, car_age)
+    car_age = carClass.car_age
+    ngt_life = calc_ngt_life(carClass.fuel, carClass.car_age)
     ngt_critical = True if ngt_life<=3 else False
     is_lux_model = carClass.model in (LUXURY_MODELS)
 
     car_dict = {
         'model': carClass.model,
-        'fuel': carClass.fuel, 
+        'Fuel': carClass.fuel, 
         'year': carClass.year, 
-        'transmission': carClass.transmission, 
-        'km_driven': carClass.km_driven, 
-        'eng_capacity': carClass.eng_capacity, 
-        'ownership': carClass.ownership, 
+        'Transmission': carClass.transmission, 
+        'KM driven': carClass.km_driven, 
+        'Engine capacity': carClass.eng_capacity, 
+        'Ownership': carClass.ownership, 
         'asking_price': carClass.asking_price,
         'car_age': car_age,
         'ngt_life': ngt_life,
         'ngt_critical': ngt_critical,
-        'is_lux_model':  is_lux_model
+        'is_luxury_brand':  is_lux_model
     }
     car_df = pd.DataFrame([car_dict])
     car_df['model'] = car_df['model'].astype('category')
-    car_df['fuel'] = car_df['fuel'].astype('category')
-    car_df['transmission'] = car_df['transmission'].astype('category')
+    car_df['Fuel'] = car_df['Fuel'].astype('category')
+    car_df['Transmission'] = car_df['Transmission'].astype('category')
 
     return(car_df)
 
